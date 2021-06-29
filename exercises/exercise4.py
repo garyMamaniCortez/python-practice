f9ef79d84c6298ac50205481a114a26122eac8b4
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-06-29 15:41:02 -0400

    feat: exercise 4 get the final price of a product with 36% off

---
+++ exercises/exercise4.py
@@ -0,0 +1,5 @@
+# get the final price that you have to paid for 
+# a product with 36% off
+price= float(input("insert the price: "))
+discount= price*0.36
+print(f"the final price is: {price-discount}")
