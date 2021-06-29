9be2ffd201b0abcce5a6173105b6e6f1c813397c
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-06-29 18:47:07 -0400

    feat: condicionals if, elif, else in python

---
+++ condicional.py
@@ -0,0 +1,8 @@
+number= int(input("ingrese una numero: "))
+
+if number>0:
+    print("el numero es positivo")
+elif number<0:
+    print("el numero es negativo")
+else:
+    print("el numero es nulo")
