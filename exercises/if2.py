c9066956dd425c869d95cc00b2247d1feae90622
garyMamaniCortez <mamanijhonny2210@gmail.com>
2021-06-29 19:04:01 -0400

    feat: second exercise with conditionals in python

---
+++ exercises/if2.py
@@ -0,0 +1,9 @@
+a=float(input("insert number 1: "))
+b=float(input("insert number 2: "))
+c=float(input("insert number 3: "))
+if a>=b and a>=c:
+    print("number 1 is higher")
+elif b>=a and b>=c:
+    print("number 2 is higher")
+else:
+    print("number 3 is higher")
